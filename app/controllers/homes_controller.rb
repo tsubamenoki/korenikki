0ca65e0e92a439acb107e383df2c67848f62b53d
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-12-16 17:40:02 +0000

    memo add

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,2 +1,13 @@
 class HomesController < ApplicationController
+  before_action :authenticate_user!, except: [:about]
+
+  def top
+    @post = Post.last
+    @post_comment = PostComment.new
+    @post_comments = PostComment.all
+    @today_comment = @post_comments.created_today
+    @yesterday_comment = @post_comments.created_yesterday
+    @this_week_comment = @post_comments.created_this_week
+    @last_week_commnet = @post_comments.created_last_week
+  end
 end
