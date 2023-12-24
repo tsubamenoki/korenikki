334168cdf1f45feb72f4450bac800de9cde25b7d
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-12-24 11:35:11 +0000

    commit

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,6 +2,6 @@ class Tag < ApplicationRecord
 
   has_many :tag_relationships, dependent: :destroy
   has_many :posts, through: :tag_relationships
-  
-  validates :name, presence: true, uniqueness: true
+
+  validates :name, presence: true
 end
