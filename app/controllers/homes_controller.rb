0098805900923ed1515abf1f12173675d1e7ffea
tsubamenoki <144215790+tsubamenoki@users.noreply.github.com>
2023-12-25 08:24:03 +0000

    tag count

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,15 +4,16 @@ class HomesController < ApplicationController
   def top
     @post = current_user.posts.last
     unless @post == nil
+      #コメント機能
       @post_comment = PostComment.new
       @post_comments = PostComment.all
+      #コメントグラフ
       @today_comment = @post_comments.created_today
       @yesterday_comment = @post_comments.created_yesterday
       @this_week_comment = @post_comments.created_this_week
       @last_week_commnet = @post_comments.created_last_week
-      @tags = @post.tags.pluck(:name).join(',')
+      #タグ関連
       @post_tags = @post.tags
-      @posts = Post.all
       tag_ids = TagRelationship.where(post_id: current_user.posts.pluck(:id)).pluck(:tag_id)
       @tag_lists = Tag.where(id: tag_ids)
     end
